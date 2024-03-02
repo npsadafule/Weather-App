665c7809cc379782c37e44d889acbfd8fb0b738d
Neel Sadafule <neel.sadafule13@gmail.com>
2024-03-02 01:36:32 -0800

    Added location search functionality

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { View, SafeAreaView, TextInput, TouchableOpacity, Image, StyleSheet, Dimensions } from 'react-native';
+import { View, SafeAreaView, TextInput, TouchableOpacity, Image, StyleSheet, Dimensions, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { theme } from '../theme';
 import { MagnifyingGlassIcon } from 'react-native-heroicons/outline';
-
+import {MapPinIcon } from 'react-native-heroicons/solid';
 const { width, height } = Dimensions.get('window');
 
 export default function HomeScreen() {
     const [showSearch, toggleSearch] = useState(false);
+    const [locations, setLocations] = useState([1,2,3]);
     return (
         <View style={styles.container}>
             <StatusBar style="light" />
@@ -36,6 +37,27 @@ export default function HomeScreen() {
                             <MagnifyingGlassIcon color="white" size={25} />
                         </TouchableOpacity>
                     </View>
+                    {
+                        locations.length > 0 && showSearch?(
+                            <View className="absolute w-full bg-gray-300 top-16 rounded-3xl">
+                                {
+                                    locations.map((location, index) => {
+                                        let showBorder = index+1 != locations.length;
+                                        let borderClass = showBorder? 'border-b-2-gray-400': '';
+                                        return (
+                                            <TouchableOpacity
+                                                key = {index}
+                                                className={"flex-row items-center justify-between border-0 p-3 px-4 mb-1 " + borderClass }
+                                            >
+                                                <MapPinIcon size="20" color="gray" />
+                                                <Text className = "text-black text-lg ml-2">London, United Kingdom</Text>
+                                            </TouchableOpacity>
+                                        )
+                                    })
+                                }
+                            </View>
+                        ):null
+                    }
                 </View>
             </SafeAreaView>
         </View>
